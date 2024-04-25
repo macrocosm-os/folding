2eae8221347cf0e90f731a44dbc1cf60a387acd9
mccrindlebrian <mccrinbc@gmail.com>
2024-04-25 13:27:42 -0500

    move miners around

--- neurons/miner.py
+++ neurons/miner.py
@@ -1,7 +1,6 @@
 # The MIT License (MIT)
-# Copyright © 2023 Yuma Rao
-# TODO(developer): Set your name
-# Copyright © 2023 <your name>
+# Developer: Macrocosmos
+# Copyright © 2024 Macrocosmos
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 # documentation files (the “Software”), to deal in the Software without restriction, including without limitation
@@ -16,69 +15,9 @@
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-
-import os
-import time
-import typing
+import time 
 import bittensor as bt
-
-# import base miner class which takes care of most of the boilerplate
-from folding.base.miner import BaseMinerNeuron
-from folding.protocol import FoldingSynapse
-from folding.miners.forward import forward
-
-# root level directory for the project (I HATE THIS)
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-
-class Miner(BaseMinerNeuron):
-    def __init__(self, config=None):
-        super(Miner, self).__init__(config=config)
-
-    async def forward(self, synapse: FoldingSynapse) -> FoldingSynapse:
-        forward(synapse=synapse, config=self.config)
-        self.step += 1
-
-        return synapse
-
-    async def blacklist(self, synapse: FoldingSynapse) -> typing.Tuple[bool, str]:
-        uid = self.metagraph.hotkeys.index(synapse.dendrite.hotkey)
-        if (
-            not self.config.blacklist.allow_non_registered
-            and synapse.dendrite.hotkey not in self.metagraph.hotkeys
-        ):
-            # Ignore requests from un-registered entities.
-            bt.logging.trace(
-                f"Blacklisting un-registered hotkey {synapse.dendrite.hotkey}"
-            )
-            return True, "Unrecognized hotkey"
-
-        if self.config.blacklist.force_validator_permit:
-            # If the config is set to force validator permit, then we should only allow requests from validators.
-            if not self.metagraph.validator_permit[uid]:
-                bt.logging.warning(
-                    f"Blacklisting a request from non-validator hotkey {synapse.dendrite.hotkey}"
-                )
-                return True, "Non-validator hotkey"
-
-        bt.logging.trace(
-            f"Not Blacklisting recognized hotkey {synapse.dendrite.hotkey}"
-        )
-        return False, "Hotkey recognized!"
-
-    async def priority(self, synapse: FoldingSynapse) -> float:
-        # TODO(developer): Define how miners should prioritize requests.
-        caller_uid = self.metagraph.hotkeys.index(
-            synapse.dendrite.hotkey
-        )  # Get the caller index.
-        priority = float(
-            self.metagraph.S[caller_uid]
-        )  # Return the stake as the priority.
-        bt.logging.trace(
-            f"Prioritizing {synapse.dendrite.hotkey} with value: ", priority
-        )
-        return priority
-
+from folding.miners.folding_miner import FoldingMiner
 
 # This is the main function, which runs the miner.
 if __name__ == "__main__":
