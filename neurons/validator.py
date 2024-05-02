f03b9bb3054b81b750078493b453531aad93ccf9
steffencruz <steffenjcruz@gmail.com>
2024-05-02 21:49:48 +0100

    Make forward method synchronous, and move the rewarding mechanism into validator update_job (dont love this deisgn but we will fix later)

--- neurons/validator.py
+++ neurons/validator.py
@@ -47,7 +47,7 @@ class Validator(BaseValidatorNeuron):
         # TODO: Change the store to SQLiteJobStore if you want to use SQLite
         self.store = PandasJobStore(db_path=self.config.neuron.db_path_location)
 
-    async def forward(self, job: Job):
+    def forward(self, job: Job) -> dict:
         """Carries out a query to the miners to check their progress on a given job (pdb) and updates the job status based on the results.
 
         Validator forward pass. Consists of:
@@ -63,14 +63,12 @@ class Validator(BaseValidatorNeuron):
 
         # TODO: the command below should correctly prepare the md_inputs to point at the current best gro files (+ others)
         bt.logging.warning("INSIDE OF THE VALIDATOR ASYNC FORWARD")
-        
         protein = Protein.from_pdb(pdb_id=job.pdb)
         uids = [self.metagaph.hotkeys.index(hotkey) for hotkey in job.hotkeys]
         # query the miners and get the rewards for their responses
         # Check check_uid_availability to ensure that the hotkeys are valid and active
-        
         bt.logging.info("⏰ Waiting for miner responses ⏰")
-        return await run_step(
+        return run_step(
             self,
             protein=protein,
             uids=uids,
@@ -84,14 +82,11 @@ class Validator(BaseValidatorNeuron):
         Args:
             k (int): The number of jobs create and distribute to miners.
         """
-        
-        #Set of pdbs that are currently in the process of running. 
+        #Set of pdbs that are currently in the process of running.
         exclude_pdbs = [queued_job.pdb for queued_job in self.store.get_queue(ready=False).queue]
-        
         # Deploy K number of unique pdb jobs, where each job gets distributed to self.config.neuron.sample_size miners
         for ii in range(k):
             bt.logging.error(f"Adding job: {ii+1}/{k}")
-            
             # selects a new pdb, downloads data, preprocesses and gets hyperparams.
             job_event: Dict = create_new_challenge(self, exclude=exclude_pdbs)
 
@@ -99,7 +94,6 @@ class Validator(BaseValidatorNeuron):
             active_jobs = self.store.get_queue(ready=False).queue
             active_hotkeys = [j.hotkeys for j in active_jobs]
             active_hotkeys = list(chain.from_iterable(active_hotkeys))
-            
             exclude_uids = [
                 self.metagaph.hotkeys.index(hotkey) for hotkey in active_hotkeys
             ]
@@ -123,16 +117,41 @@ class Validator(BaseValidatorNeuron):
         TODO: we also need to remove hotkeys that have not participated for some time (dereg or similar)
         """
 
-        # set updated_at to most recent query
-        # maybe attach some metadata about the specific checkpoints that are the current head
-        loss = event["best_loss"]
-        hotkey = event["best_hotkey"]
+        losses = event['losses']
+
+        best_index = np.argmin(losses)
+        best_loss = losses[best_index]
+        best_hotkey = job.hotkeys[best_index]
+
+        print(f'{best_index=}, {best_loss=}, {best_hotkey=}')
+
+        # TODO: we need to get the commit and gro hashes from the best hotkey
         commit_hash = ""  # For next time
         gro_hash = ""  # For next time
 
-        # check if early stopping criteria is met
-        job.update(loss=loss, hotkey=hotkey, commit_hash=commit_hash, gro_hash=gro_hash)
+        # This does a bunch of important things:
+        # 1. checks if best loss in this round is the best loss overall and updates the best_hotkey and hashes accordingly
+        # 2. Potentially applies early stopping criteria
+        # 3. Ensures that all timestamps and counters are updated correctly
+        job.update(
+            loss=best_loss,
+            hotkey=best_hotkey,
+            commit_hash=commit_hash,
+            gro_hash=gro_hash,
+        )
 
+        # one hot rewards (1 for winner, 0 for everyone else)
+        rewards = np.zeros_like(losses)
+        # note that the reward goes to current leader (even if they didn't do well this round)
+        rewards[best_index] = job.hotkeys.index(job.best_hotkey)
+
+        uids = [self.metagraph.hotkeys.index(hotkey) for hotkey in job.hotkeys]
+        self.update_scores(
+            rewards=rewards,
+            uids=uids,  # pretty confident these are in the correct order.
+        )
+
+        # Finally, we update the job in the store
         self.store.update(job=job)
 
 
