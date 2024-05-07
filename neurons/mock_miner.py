c9987bfb5fdb118adf97fe2e00501ea0205a0fe6
mccrindlebrian <mccrinbc@gmail.com>
2024-05-07 13:42:24 -0500

    mock miner script

---
+++ neurons/mock_miner.py
@@ -0,0 +1,29 @@
+# The MIT License (MIT)
+# Developer: Macrocosmos
+# Copyright © 2024 Macrocosmos
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF ME3CHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+import time
+import bittensor as bt
+from folding.miners.mock_miner import MockFoldingMiner
+
+# This is the main function, which runs the miner.
+if __name__ == "__main__":
+    with MockFoldingMiner() as m:
+        while True:
+            bt.logging.info(
+                f"Miner running:: network: {m.subtensor.network} | step: {m.step} | uid: {m.uid} | trust: {m.metagraph.trust[m.uid]:.3f} | emission {m.metagraph.emission[m.uid]:.3f}"
+            )
+            time.sleep(30)
