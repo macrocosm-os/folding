b4a4f0d183d7b86104f108f38e53a20966d3d675
bkb2135 <bkb2135@columbia.edu>
2024-05-13 03:43:38 +0000

    Add scripts for quick start

---
+++ scripts/run_miner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source .venv/bin/activate
+
+# Execute the Python script
+python ../neurons/miner.py \
+    --netuid 141 \
+    --subtensor.network test \
+    --wallet.name test_wallet \
+    --wallet.hotkey test_hotkey \
+    --axon.port 8091
