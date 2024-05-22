62e4d3f2cf862bfd18da50aa8143ab820a39054e
mccrindlebrian <mccrinbc@gmail.com>
2024-05-22 12:54:57 -0500

    change version spec in folding

--- folding/__init__.py
+++ folding/__init__.py
@@ -4,7 +4,7 @@ from .validators.protein import Protein
 __version__ = "0.0.1"
 version_split = __version__.split(".")
 __spec_version__ = (
-    (1000 * int(version_split[0]))
-    + (10 * int(version_split[1]))
+    (10000 * int(version_split[0]))
+    + (100 * int(version_split[1]))
     + (1 * int(version_split[2]))
 )
