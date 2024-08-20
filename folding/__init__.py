ce2cff446f30538816eef0ad23efad17d230d37f
mccrindlebrian <mccrinbc@gmail.com>
2024-08-20 10:14:52 -0500

    increase version to 0.3.1

--- folding/__init__.py
+++ folding/__init__.py
@@ -1,7 +1,7 @@
 from .protocol import JobSubmissionSynapse
 from .validators.protein import Protein
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 version_split = __version__.split(".")
 __spec_version__ = (
     (10000 * int(version_split[0]))
