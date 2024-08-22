a9e324b166a31a0bfc0260be57b5bff7907c3234
mccrindlebrian <mccrinbc@gmail.com>
2024-08-21 21:24:44 -0500

    add OpenMMForceField classes to define how we should index them

---
+++ folding/utils/openmm_forcefields.py
@@ -0,0 +1,94 @@
+from abc import ABC, abstractmethod
+
+
+class OpenMMForceField(ABC):
+    def __init__(self) -> None:
+        pass
+
+    @property
+    @abstractmethod
+    def name(self):
+        ...
+
+    @property
+    @abstractmethod
+    def recommended_configuration(self):
+        ...
+
+    @abstractmethod
+    def entities(self):
+        ...
+
+    @abstractmethod
+    def waters(self):
+        ...
+
+
+class Amber14(OpenMMForceField):
+    # ref: http://docs.openmm.org/latest/userguide/application/02_running_sims.html?highlight=gaff#amber14
+
+    @property
+    def name(self):
+        return "amber14"
+
+    @property
+    def recommended_configuration(self):
+        return ["amber14-all.xml", "amber14/tip3pfb.xml"]
+
+    def entities(self):
+        forces = [
+            "protein.ff14SB.xml",
+            "protein.ff15ipq.xml",
+            "DNA.OL15.xml",
+            "DNA.bsc1.xml",
+            "RNA.OL3.xml",
+            "lipid17.xml",
+            "GLYCAM_06j-1.xml",
+        ]
+        return [f"{self.name}/{force}" for force in forces]
+
+    def waters(self):
+        waters = [
+            "tip3p.xml",
+            "tip3pfb.xml",
+            "tip4pew.xml",
+            "tip4pfb.xml",
+            "spce.xml",
+            "opc.xml",
+            "opc3.xml",
+        ]
+        return [f"{self.name}/{water}" for water in waters]
+
+
+class Charmm36(OpenMMForceField):
+    # ref: http://docs.openmm.org/latest/userguide/application/02_running_sims.html?highlight=gaff#charmm36
+
+    @property
+    def name(self):
+        return "charmm36"
+
+    @property
+    def recommended_configuration(self):
+        return ["charmm36.xml", "charmm36/water.xml"]
+
+    def entities(self):
+        forces = [
+            "charmm36.xml",
+        ]
+        return forces
+
+    def waters(self):
+        waters = [
+            "water.xml",
+            "spce.xml",
+            "tip3p-pme-b.xml",
+            "tip3p-pme-f.xml",
+            "tip4pew.xml",
+            "tip4p2005.xml",
+            "tip5p.xml",
+            "tip5pew.xml",
+        ]
+        return [f"{self.name}/{water}" for water in waters]
+
+
+FORCEFIELD_REGISTERY = {"amber14": Amber14, "charmm36": Charmm36}
