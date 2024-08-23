46df849fc3ed6130b01fda250279759f05b6263f
mccrindlebrian <mccrinbc@gmail.com>
2024-08-22 20:52:54 -0500

    add merge config method

--- folding/base/validator.py
+++ folding/base/validator.py
@@ -16,7 +16,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-import json 
+import json
 import copy
 import time
 import torch
@@ -79,7 +79,9 @@ class BaseValidatorNeuron(BaseNeuron):
         self.is_running: bool = False
         self.thread: threading.Thread = None
         self.lock = asyncio.Lock()
-        self.json_config = self.load_config_json() # Load the config json file using the new method 
+        self.json_config = (
+            self.load_config_json()
+        )  # Load the config json file using the new method
 
     def serve_axon(self):
         """Serve axon to enable external connections."""
@@ -168,11 +170,11 @@ class BaseValidatorNeuron(BaseNeuron):
                     # Here we straightforwardly query the workers associated with each job and update the jobs accordingly
                     job_event = self.forward(job=job)
 
-                    # If we don't have any miners reply to the query, we will make it inactive. 
-                    if len(job_event["energies"]) == 0: 
+                    # If we don't have any miners reply to the query, we will make it inactive.
+                    if len(job_event["energies"]) == 0:
                         job.active = False
                         self.store.update(job=job)
-                        continue 
+                        continue
 
                     if isinstance(job.event, str):
                         job.event = eval(job.event)  # if str, convert to dict.
@@ -400,8 +402,18 @@ class BaseValidatorNeuron(BaseNeuron):
         self.scores = state["scores"]
         self.hotkeys = state["hotkeys"]
 
-    def load_config_json(self): 
-        config_json_path = '/home/paperspace/folding/folding/utils/config_input.json' # New method to load the config json file
-        with open(config_json_path, 'r') as file: 
-            config = json.load(file)    
+    def load_config_json(self):
+        config_json_path = "/home/paperspace/folding/folding/utils/config_input.json"  # New method to load the config json file
+        with open(config_json_path, "r") as file:
+            config = json.load(file)
         return config
+
+    def merge_configs(self):
+        merged_config = self.load_config_json()
+
+        # Copy the existing config from the parent classes
+        merged_config.update(self.config)
+
+        # Update the new config into the existing config
+        # config_json.update(new_config)
+        return merged_config
