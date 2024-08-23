6148403bff41e02ae7b7ad89d628a04b97e52b66
mccrindlebrian <mccrinbc@gmail.com>
2024-08-22 19:40:54 -0500

    move openmm config into new util

--- folding/utils/opemm_simulation_config.py
+++ folding/utils/opemm_simulation_config.py
@@ -1,19 +1,20 @@
-from typing import List, Dict, Literal, Optional
-import openmm as mm
+from typing import Literal, Optional
 from openmm import app
-from openmm import unit
 from enum import Enum
 from pydantic import BaseModel
+
 
 class NonbondedMethod(Enum):
     PME = app.PME
     NoCutoff = app.NoCutoff
+
 
 class Constraints(Enum):
     Unconstricted = None
     HBonds = app.HBonds
     AllBonds = app.AllBonds
     HAngles = app.HAngles
+
 
 class SimulationConfig(BaseModel):
     ff: str
@@ -32,16 +33,17 @@ class SimulationConfig(BaseModel):
     pressure: float = 1.0
     max_steps_nvt: int = 50000
 
-    emtol: float = 1000.0  
-    nsteps_minimize: int = 100  
-    rvdw: float = 1.2  
-    rcoulomb: float = 1.2  
-    fourier_spacing: float = 0.15  
-    tau_p: float = 5.0 
-    ref_p: float = 1.0  
-    ref_t: float = 300.0  
-    gen_temp: float = 300.0 
+    emtol: float = 1000.0
+    nsteps_minimize: int = 100
+    rvdw: float = 1.2
+    rcoulomb: float = 1.2
+    fourier_spacing: float = 0.15
+    tau_p: float = 5.0
+    ref_p: float = 1.0
+    ref_t: float = 300.0
+    gen_temp: float = 300.0
 
+    # TODO: Do we need this?
     def apply_input(self):
         class Config:
             arbitrary_types_allowed = True
