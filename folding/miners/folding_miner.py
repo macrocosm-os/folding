cfd7c7a84271f4050dcc3b53e49c9c306d57e981
mccrindlebrian <mccrinbc@gmail.com>
2024-09-23 14:14:41 -0400

    merge folding miner from openmm branch

--- folding/miners/folding_miner.py
+++ folding/miners/folding_miner.py
@@ -1,19 +1,18 @@
-import base64
-import concurrent.futures
-import glob
 import os
-import random
 import time
+import glob
+import base64
+import random
+import hashlib
+import concurrent.futures
 from collections import defaultdict
 from typing import Dict, List, Tuple
-from sys import stdout
 import copy
-
+import traceback
 
 import bittensor as bt
 import openmm as mm
 import openmm.app as app
-import openmm.unit as unit
 
 # import base miner class which takes care of most of the boilerplate
 from folding.base.miner import BaseMinerNeuron
@@ -171,22 +170,61 @@ class FoldingMiner(BaseMinerNeuron):
         if len(self.simulations) > 0:
             sims_to_delete = []
 
-            for pdb_id, simulation in self.simulations.items():
-                current_executor_state = simulation["executor"].get_state()
+            for pdb_hash, simulation in self.simulations.items():
+                future = simulation["future"]
+                pdb_id = simulation["pdb_id"]
 
-                if current_executor_state == "finished":
-                    bt.logging.warning(
-                        f"✅ {pdb_id} finished simulation... Removing from execution stack ✅"
-                    )
-                    sims_to_delete.append(pdb_id)
+                # Check if the future is done
+                if future.done():
+                    state, error_info = future.result()
+                    # If the simulation is done, we can remove it from the simulation store
+                    if state == "finished":
+                        bt.logging.warning(
+                            f"✅ {pdb_id} finished simulation... Removing from execution stack ✅"
+                        )
+                    else:
+                        # If the simulation failed, we should log the error and remove it from the simulation store
+                        bt.logging.error(
+                            f"❗ {pdb_id} failed simulation... Removing from execution stack ❗"
+                        )
+                        bt.logging.error(f"Error info: {error_info}")
+                    sims_to_delete.append(pdb_hash)
 
-            for pdb_id in sims_to_delete:
-                del self.simulations[pdb_id]
+            for pdb_hash in sims_to_delete:
+                del self.simulations[pdb_hash]
 
-            event["running_simulations"] = list(self.simulations.keys())
-            bt.logging.warning(f"Simulations Running: {list(self.simulations.keys())}")
+            running_simulations = [sim["pdb_id"] for sim in self.simulations.values()]
+
+            event["running_simulations"] = running_simulations
+            bt.logging.warning(f"Simulations Running: {running_simulations}")
 
         return event
+
+    def get_simulation_hash(self, pdb_id: str, system_config: Dict) -> str:
+        """Creates a simulation hash based on the pdb_id and the system_config given.
+
+        Returns:
+            str: first 6 characters of a sha256 hash
+        """
+        system_hash = pdb_id
+        for key, value in system_config.items():
+            system_hash += str(key) + str(value)
+
+        hash_object = hashlib.sha256(system_hash.encode("utf-8"))
+        return hash_object.hexdigest()[:6]
+
+    def is_unique_job(self, system_config_filepath: str) -> bool:
+        """Check to see if a submitted job is unique by checking to see if the folder exists.
+
+        Args:
+            system_config_filepath (str): filepath for the config file that specifies the simulation
+
+        Returns:
+            bool
+        """
+        if os.path.exists(system_config_filepath):
+            return False
+        return True
 
     def forward(self, synapse: JobSubmissionSynapse) -> JobSubmissionSynapse:
         """
@@ -215,15 +253,45 @@ class FoldingMiner(BaseMinerNeuron):
         event = self.create_default_dict()
         event["pdb_id"] = pdb_id
 
-        output_dir = os.path.join(self.base_data_path, pdb_id)
+        pdb_hash = self.get_simulation_hash(
+            pdb_id=pdb_id, system_config=synapse.system_config
+        )
+        output_dir = os.path.join(self.base_data_path, pdb_id, pdb_hash)
+        system_config_filepath = os.path.join(output_dir, f"config_{pdb_id}.pkl")
 
         # check if any of the simulations have finished
         event = self.check_and_remove_simulations(event=event)
+        submitted_job_is_unique = self.is_unique_job(
+            system_config_filepath=system_config_filepath
+        )
+
+        if submitted_job_is_unique:
+            if len(self.simulations) < self.max_workers:
+                return self.submit_simulation(
+                    synapse=synapse,
+                    pdb_id=pdb_id,
+                    pdb_hash=pdb_hash,
+                    output_dir=output_dir,
+                    system_config_filepath=system_config_filepath,
+                    event=event,
+                )
+
+            elif len(self.simulations) == self.max_workers:
+                bt.logging.warning(
+                    f"❗ Cannot start new process: job limit reached. ({len(self.simulations)}/{self.max_workers}).❗"
+                )
+
+                bt.logging.warning(f"❗ Removing miner from job pool ❗")
+
+                event["condition"] = "cpu_limit_reached"
+                synapse.miner_serving = False
+
+                return check_synapse(self=self, synapse=synapse, event=event)
 
         # The set of RUNNING simulations.
-        if pdb_id in self.simulations:
-            self.simulations[pdb_id]["queried_at"] = time.time()
-            simulation = self.simulations[pdb_id]
+        elif pdb_hash in self.simulations:
+            self.simulations[pdb_hash]["queried_at"] = time.time()
+            simulation = self.simulations[pdb_hash]
             current_executor_state = simulation["executor"].get_state()
             current_seed = simulation["executor"].seed
 
@@ -258,6 +326,17 @@ class FoldingMiner(BaseMinerNeuron):
                         state = lines[-1].strip()
                         state = "md_0_1" if state == "finished" else state
 
+                    # If the state is failed, we should not return the files.
+                    if state == "failed":
+                        synapse.miner_state = state
+                        synapse.miner_serving = False
+                        event["condition"] = "failed_simulation"
+                        event["state"] = state
+                        bt.logging.warning(
+                            f"❗Returning previous simulation data for failed simulation: {pdb_id}❗"
+                        )
+                        return check_synapse(self=self, synapse=synapse, event=event)
+
                     with open(seed_file, "r") as f:
                         seed = f.readlines()[-1].strip()
 
@@ -276,27 +355,28 @@ class FoldingMiner(BaseMinerNeuron):
                     )
                     state = None
 
-                event["condition"] = "found_existing_data"
-                event["state"] = state
+                finally:
+                    event["condition"] = "found_existing_data"
+                    event["state"] = state
 
-                return check_synapse(self=self, synapse=synapse, event=event)
-
-            elif len(self.simulations) >= self.max_workers:
-                bt.logging.warning(
-                    f"❗ Cannot start new process: job limit reached. ({len(self.simulations)}/{self.max_workers}).❗"
-                )
-
-                bt.logging.warning(f"❗ Removing miner from job pool ❗")
-
-                event["condition"] = "cpu_limit_reached"
-                synapse.miner_serving = False
-
-                return check_synapse(self=self, synapse=synapse, event=event)
+                    return check_synapse(self=self, synapse=synapse, event=event)
 
             elif len(synapse.md_inputs) == 0:  # The vali sends nothing to the miner
                 return check_synapse(self=self, synapse=synapse, event=event)
+
+    def submit_simulation(
+        self,
+        synapse: JobSubmissionSynapse,
+        output_dir: str,
+        pdb_id: str,
+        pdb_hash: str,
+        system_config_filepath: str,
+        event: Dict,
+    ):
         # Make sure the output directory exists and if not, create it
         check_if_directory_exists(output_directory=output_dir)
+
+        # We are going to use a hash based on the pdb_id and the system config to index the simulation dict
 
         # The following files are required for openmm simulations and are received from the validator
         for filename, content in synapse.md_inputs.items():
@@ -316,8 +396,8 @@ class FoldingMiner(BaseMinerNeuron):
         with open(os.path.join(output_dir, f"{pdb_id}.pdb"), "w") as f:
             f.write(synapse.pdb_contents)
 
-        system_config = SimulationConfig(**system_config)
-        write_pkl(system_config, os.path.join(output_dir, f"config_{pdb_id}.pkl"))
+        system_config = SimulationConfig(**synapse.system_config)
+        write_pkl(system_config, system_config_filepath)
 
         # Create the job and submit it to the executor
         simulation_manager = SimulationManager(
@@ -334,10 +414,11 @@ class FoldingMiner(BaseMinerNeuron):
             self.config.mock or self.mock,  # self.mock is inside of MockFoldingMiner
         )
 
-        self.simulations[pdb_id]["executor"] = simulation_manager
-        self.simulations[pdb_id]["future"] = future
-        self.simulations[pdb_id]["output_dir"] = simulation_manager.output_dir
-        self.simulations[pdb_id]["queried_at"] = time.time()
+        self.simulations[pdb_hash]["pdb_id"] = pdb_id
+        self.simulations[pdb_hash]["executor"] = simulation_manager
+        self.simulations[pdb_hash]["future"] = future
+        self.simulations[pdb_hash]["output_dir"] = simulation_manager.output_dir
+        self.simulations[pdb_hash]["queried_at"] = time.time()
 
         bt.logging.success(f"✅ New pdb_id {pdb_id} submitted to job executor ✅ ")
 
@@ -393,10 +474,11 @@ class SimulationManager:
         self.pdb_id = pdb_id
         self.state: str = None
         self.seed = seed
-        self.pdb_obj = app.PDBFile(f"{output_dir}/{pdb_id}.pdb")
+        self.pdb_obj = app.PDBFile(os.path.join(output_dir, f"{pdb_id}.pdb"))
 
         self.state_file_name = f"{pdb_id}_state.txt"
         self.seed_file_name = f"{pdb_id}_seed.txt"
+        self.simulation_steps: dict = system_config["simulation_steps"]
         self.system_config = SimulationConfig(**system_config)
 
         self.output_dir = output_dir
@@ -418,6 +500,10 @@ class SimulationManager:
         with open(file_path, "w") as f:
             pass
 
+    def write_state(self, state: str, state_file_name: str, output_dir: str):
+        with open(os.path.join(output_dir, state_file_name), "w") as f:
+            f.write(f"{state}\n")
+
     def run(
         self,
         mock: bool = False,
@@ -430,35 +516,81 @@ class SimulationManager:
             mock (bool, optional): mock for debugging. Defaults to False.
         """
         bt.logging.info(f"Running simulation for protein: {self.pdb_id}")
+        simulations = self.configure_commands(
+            seed=self.seed, system_config=copy.deepcopy(self.system_config)
+        )
+        bt.logging.info(f"Simulations: {simulations}")
 
         # Make sure the output directory exists and if not, create it
         check_if_directory_exists(output_directory=self.output_dir)
         os.chdir(self.output_dir)
 
-        steps = {"nvt": 50000, "npt": 75000, "md_0_1": 100000}
-
         # Write the seed so that we always know what was used.
         with open(self.seed_file_name, "w") as f:
             f.write(f"{self.seed}\n")
 
-        for state in self.STATES:
-            simulation = self.configure_commands(
-                state=state, seed=self.seed, system_config=self.system_config
+        try:
+            for state, simulation in simulations.items():
+                bt.logging.info(f"Running {state} commands")
+
+                self.write_state(
+                    state=state,
+                    state_file_name=self.state_file_name,
+                    output_dir=self.output_dir,
+                )
+
+                simulation.loadCheckpoint(self.cpt_file_mapper[state])
+                simulation.step(self.simulation_steps[state])
+
+                # TODO: Add a Mock pipeline for the new OpenMM simulation here.
+
+            bt.logging.success(f"✅ Finished simulation for protein: {self.pdb_id} ✅")
+
+            state = "finished"
+            self.write_state(
+                state=state,
+                state_file_name=self.state_file_name,
+                output_dir=self.output_dir,
             )
+            return state, None
 
-            with open(os.path.join(self.output_dir, self.state_file_name), "w") as f:
-                f.write(f"{state}\n")
+        # This is the exception that is raised when the simulation fails.
+        except mm.OpenMMException as e:
+            state = "failed"
+            error_info = {
+                "type": "OpenMMException",
+                "message": str(e),
+                "traceback": traceback.format_exc(),  # This is the traceback of the exception
+            }
+            try:
+                platform = mm.Platform.getPlatformByName("CUDA")
+                error_info["cuda_version"] = platform.getPropertyDefaultValue(
+                    "CudaCompiler"
+                )
+            except:
+                error_info["cuda_version"] = "Unable to get CUDA information"
+            finally:
+                self.write_state(
+                    state=state,
+                    state_file_name=self.state_file_name,
+                    output_dir=self.output_dir,
+                )
+                return state, error_info
 
-            simulation.loadCheckpoint(self.cpt_file_mapper[state])
-            simulation.step(steps[state])
-
-            # TODO: Add a Mock pipeline for the new OpenMM simulation here.
-
-        bt.logging.success(f"✅ Finished simulation for protein: {self.pdb_id} ✅")
-
-        state = "finished"
-        with open(self.state_file_name, "w") as f:
-            f.write(f"{state}\n")
+        # Generic Exception
+        except Exception as e:
+            state = "failed"
+            error_info = {
+                "type": "UnexpectedException",
+                "message": str(e),
+                "traceback": traceback.format_exc(),
+            }
+            self.write_state(
+                state=state,
+                state_file_name=self.state_file_name,
+                output_dir=self.output_dir,
+            )
+            return state, error_info
 
     def get_state(self) -> str:
         """get_state reads a txt file that contains the current state of the simulation"""
@@ -474,37 +606,41 @@ class SimulationManager:
             return lines[-1].strip() if lines else None
 
     def configure_commands(
-        self, state: str, seed: int, system_config: dict
+        self, seed: int, system_config: SimulationConfig
     ) -> Dict[str, List[str]]:
-        simulation, _ = OpenMMSimulation().create_simulation(
-            pdb=self.pdb_obj,
-            system_config=system_config.get_config(),
-            seed=seed,
-            state=state,
-        )
-        simulation.reporters.append(
-            LastTwoCheckpointsReporter(
-                file_prefix=f"{self.output_dir}/{state}",
-                reportInterval=self.CHECKPOINT_INTERVAL,
-            )
-        )
-        simulation.reporters.append(
-            app.StateDataReporter(
-                file=f"{self.output_dir}/{state}.log",
-                reportInterval=self.STATE_DATA_REPORTER_INTERVAL,
-                step=True,
-                potentialEnergy=True,
-            )
-        )
-        simulation.reporters.append(
-            ExitFileReporter(
-                filename=f"{self.output_dir}/{state}",
-                reportInterval=self.EXIT_REPORTER_INTERVAL,
-                file_prefix=state,
-            )
-        )
+        state_commands = {}
 
-        return simulation
+        for state in self.STATES:
+            simulation, _ = OpenMMSimulation().create_simulation(
+                pdb=self.pdb_obj,
+                system_config=system_config.get_config(),
+                seed=seed,
+                state=state,
+            )
+            simulation.reporters.append(
+                LastTwoCheckpointsReporter(
+                    file_prefix=f"{self.output_dir}/{state}",
+                    reportInterval=self.CHECKPOINT_INTERVAL,
+                )
+            )
+            simulation.reporters.append(
+                app.StateDataReporter(
+                    file=f"{self.output_dir}/{state}.log",
+                    reportInterval=self.STATE_DATA_REPORTER_INTERVAL,
+                    step=True,
+                    potentialEnergy=True,
+                )
+            )
+            simulation.reporters.append(
+                ExitFileReporter(
+                    filename=f"{self.output_dir}/{state}",
+                    reportInterval=self.EXIT_REPORTER_INTERVAL,
+                    file_prefix=state,
+                )
+            )
+            state_commands[state] = simulation
+
+        return state_commands
 
 
 class MockSimulationManager(SimulationManager):
