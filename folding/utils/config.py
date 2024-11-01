0dc29876486b204f8fddddb51414256494c5b39f
schampoux <sergiochampoux@macrocosmos.ai>
2024-11-01 16:50:10 +0000

    change default input source to None

--- folding/utils/config.py
+++ folding/utils/config.py
@@ -208,7 +208,7 @@ def add_args(cls, parser):
         "--protein.input_source",
         type=str,
         help="Specifies the input source for selecting a new protein for simulation.",
-        default="rcsb",
+        default=None,
         choices=["rcsb", "pdbe"],
     )
 
