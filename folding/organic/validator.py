843a37c2f9a19dfe86871f3602f489812e26d418
mccrindlebrian <mccrinbc@gmail.com>
2024-11-07 11:02:33 +0000

    remove override

--- folding/organic/validator.py
+++ folding/organic/validator.py
@@ -1,4 +1,4 @@
-from typing import Any, override
+from typing import Any
 from folding.protocol import OrganicSynapse
 from atom.organic_scoring import OrganicScoringBase
 
@@ -6,7 +6,6 @@ from atom.organic_scoring import OrganicScoringBase
 class OrganicValidator(OrganicScoringBase):
     """Validator class to handle organic entries."""
 
-    @override
     async def _on_organic_entry(self, synapse: OrganicSynapse) -> None:
         config: dict = synapse.get_simulation_params()
         self._organic_queue.add(config)
