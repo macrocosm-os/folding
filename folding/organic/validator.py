2a6b6cf1a6ff65937595299d1f1b846f4bd3f6dc
mccrindlebrian <mccrinbc@gmail.com>
2024-11-07 14:31:34 +0000

    fix blacklist function

--- folding/organic/validator.py
+++ folding/organic/validator.py
@@ -1,10 +1,19 @@
+from typing import Tuple
+
 from folding.protocol import OrganicSynapse
 from atom.organic_scoring import OrganicScoringBase
 
 
 class OrganicValidator(OrganicScoringBase):
     """Validator class to handle organic entries."""
+    
+    ORGANIC_WHITELIST_HOTKEY = ''
 
     async def _on_organic_entry(self, synapse: OrganicSynapse) -> None:
         config: dict = synapse.get_simulation_params()
         self._organic_queue.add(config)
+
+    def _blacklist_fn(self, synapse: OrganicSynapse) -> Tuple[bool, str]:
+        if synapse.dendrite.hotkey != self.ORGANIC_WHITELIST_HOTKEY:
+            return True
+        return False, ""
