84903970c68dbc4072172a46e50bdfdbd4d2bcbd
mccrindlebrian <mccrinbc@gmail.com>
2024-11-08 13:42:21 +0000

    improve forward method

--- folding/organic/validator.py
+++ folding/organic/validator.py
@@ -32,6 +32,7 @@ class OrganicValidator(OrganicScoringBase):
             trigger_scaling_factor=trigger_scaling_factor,
         )
 
+        # Self reference the validator object to have access to validator methods.
         self._validator = validator
 
     async def _on_organic_entry(self, synapse: OrganicSynapse) -> None:
@@ -39,6 +40,11 @@ class OrganicValidator(OrganicScoringBase):
         self._organic_queue.add(config)
 
     async def sample(self) -> dict[str, Any]:
+        """Sample data from the organic queue or the synthetic dataset.
+
+        Returns:
+            dict[str, Any]: dict that contains all the attributes for creating a simulation object. 
+        """
         if not self._organic_queue.is_empty():
             # Choose organic sample based on the organic queue logic.
             sample = self._organic_queue.sample()
@@ -52,14 +58,22 @@ class OrganicValidator(OrganicScoringBase):
         return sample
 
     async def forward(self) -> dict[str, Any]:
+        """The forward method is responsible for sampling data from the organic queue, 
+        and adding it to the local database of the validator. 
+        """
         init_time = time.perf_counter()
-
         sample = await self.sample()
 
         if not sample:
-            return {"total_elapsed_time": time.perf_counter() - init_time}
+            return {
+                "sample" : False, 
+                "total_elapsed_time": time.perf_counter() - init_time}
+
+        # Add jobs to the sqlite database for the vali to process.
+        self._validator.add_jobs(k = 0, data = sample)
 
         return {
+            "sample": True,
             "total_elapsed_time": time.perf_counter() - init_time
         }
 
