4a8fa6cb49a86b2dc78fac70db966f1904a3e2d3
mccrindlebrian <mccrinbc@gmail.com>
2024-11-28 18:05:47 +0000

    increase to 1.2.2

--- folding/__init__.py
+++ folding/__init__.py
@@ -1,7 +1,7 @@
 from .protocol import JobSubmissionSynapse
 from .validators.protein import Protein
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 version_split = __version__.split(".")
 __spec_version__ = (
     (10000 * int(version_split[0]))
