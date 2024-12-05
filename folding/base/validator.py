4499c6852969170fa3c497884270d3b8fb020beb
mccrindlebrian <mccrinbc@gmail.com>
2024-12-05 14:09:43 -0500

    properly reference FoldingBase

--- folding/base/validator.py
+++ folding/base/validator.py
@@ -1,20 +1,5 @@
 # The MIT License (MIT)
-# Copyright © 2024 Yuma Rao
 # Copyright © 2024 Macrocosmos AI
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
-# the Software.
-
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
-# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
 
 import os
 import json
@@ -25,17 +10,16 @@ import argparse
 import threading
 import bittensor as bt
 from pathlib import Path
-from folding.utils.logger import logger
-
 from typing import List, Optional
 
 from atom.base.neuron import BaseNeuron as AtomBaseNeuron
 
-from folding.base import BaseFolding
 from folding.mock import MockDendrite
+from folding.utils.logger import logger
+from folding.base.neuron import BaseFolding
+from folding.utils.ops import print_on_retry
 from folding.utils.config import add_validator_args
 from folding.organic.validator import OrganicValidator
-from folding.utils.ops import print_on_retry
 
 from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_result
 
