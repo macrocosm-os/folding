468491ea7750a8873d1b466d2ccd701bd84b6a5f
schampoux <sergiochampoux@macrocosmos.ai>
2025-01-07 12:41:39 +0000

    s3 client function

--- folding/handlers/s3_client.py
+++ folding/handlers/s3_client.py
@@ -1,23 +1,32 @@
 import boto3
+import os 
+from folding.utils.logger import logger
 
+S3_CONFIG = {
+    "region_name": os.getenv("S3_REGION"),
+    "endpoint_url": os.getenv("S3_ENDPOINT"),
+    "access_key_id": os.getenv("S3_KEY"),
+    "secret_access_key": os.getenv("S3_SECRET"),
+    # "bucket_name": os.getenv("S3_BUCKET"),
+}
 
-class S3Client:
+def create_s3_client(
+    region_name: str = S3_CONFIG["region_name"],
+    endpoint_url: str = S3_CONFIG["endpoint_url"],
+    access_key_id: str = S3_CONFIG["access_key_id"],
+    secret_access_key: str = S3_CONFIG["secret_access_key"],
+) -> boto3.client:  
+    
     """Creates an S3 client"""
+    
+    if not all([region_name, endpoint_url, access_key_id, secret_access_key]):
+        raise ValueError("Missing required S3 configuration parameters.")
+    logger.info(f"Creating S3 client with region: {region_name}, endpoint: {endpoint_url}")
 
-    def __init__(self, region_name: str, endpoint_url: str, access_key_id: str, secret_access_key: str):
-        """
-        Initializes the S3 client
-        Args:
-        region_name (str): The region name
-        endpoint_url (str): The endpoint URL
-        access_key_id (str): The access key ID
-        secret_access_key (str): The secret access key
-        """
-
-        self.s3_client = boto3.session.Session().client(
-            "s3",
-            region_name=region_name,
-            endpoint_url=endpoint_url,
-            aws_access_key_id=access_key_id,
-            aws_secret_access_key=secret_access_key,
-        )
+    return boto3.session.Session().client(
+        "s3",
+        region_name=region_name,
+        endpoint_url=endpoint_url,
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+    )
