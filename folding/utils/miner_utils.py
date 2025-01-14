63500347fa788711c6ae3347a8dbde64637e02ec
schampoux <schampoux@macrocosmos.ai>
2025-01-14 17:50:42 +0000

    add self, rename to check_table

--- folding/utils/miner_utils.py
+++ folding/utils/miner_utils.py
@@ -3,13 +3,15 @@ import asyncio
 class MinerUtils: 
     def __init__(self):
         self.loop = asyncio.get_event_loop()
-    async def repeat_task():
+    async def check_table(self):
         while True:
             print("checking table")
             await asyncio.sleep(10)
+
     def start_task(self):
         # schedule the tepeat_task to run and return after 10 seconds 
-        asyncio.ensure_future(self.repeat_task(), loop=self.loop)
+        asyncio.ensure_future(self.check_table(), loop=self.loop)
+
     def run_forever(self):
         try:
             # run the event loop indefenitely
@@ -18,7 +20,9 @@ class MinerUtils:
             print("Shutting down")
         finally:
             # perform cleanup and close the loop
-            self.loop.stop()
+            if self.loop.is_running():
+                self.loop.stop()
+            self.loop.close()      
             self.loop.run_until_complete(self.loop.shutdown_asyncgens())
             self.loop.close()
             print("Shutdown complete")
