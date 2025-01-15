cb91ad9ece45d185ad1bcd321581055c0d274d3b
mccrindlebrian <mccrinbc@gmail.com>
2025-01-15 16:37:45 +0100

    v.1.4.2

--- folding/__init__.py
+++ folding/__init__.py
@@ -1,12 +1,8 @@
 from .protocol import JobSubmissionSynapse
 from .validators.protein import Protein
 
-__version__ = "1.4.0"
+__version__ = "1.4.2"
 version_split = __version__.split(".")
-__spec_version__ = (
-    (10000 * int(version_split[0]))
-    + (100 * int(version_split[1]))
-    + (1 * int(version_split[2]))
-)
+__spec_version__ = (10000 * int(version_split[0])) + (100 * int(version_split[1])) + (1 * int(version_split[2]))
 
 __OPENMM_VERSION_TAG__ = "8.2"
