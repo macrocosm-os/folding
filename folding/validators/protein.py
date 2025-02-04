084173701aeaa43a59f449b4ec2e719592809869
Sarkosos <szymonfonau@live.nl>
2025-02-04 01:35:25 +0000

    add await

--- folding/validators/protein.py
+++ folding/validators/protein.py
@@ -113,7 +113,7 @@ class Protein(OpenMMSimulation):
         self.VALIDATOR_ID = os.getenv("VALIDATOR_ID")
         try:
             self.handler = DigitalOceanS3Handler(
-                bucket_name="vali-s3-demo-do",
+                bucket_name="sn25-vali-testnet-bucket",
             )
         except ValueError as e:
             self.handler = None
