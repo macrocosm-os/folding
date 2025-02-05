2d31654e96ed45e78506da42f8e69a27af6e55cd
sarkosos <szymonfonau@live.nl>
2025-02-05 16:05:08 +0000

    removed testing script

--- scripts/run_validator.sh
+++ scripts/run_validator.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
+
 # Execute the Python script
-python ./neurons/validator.py \
-    --netuid 141 \
-    --subtensor.network test \
-    --wallet.name folding_testnet \
-    --wallet.hotkey v2 \
-    --neuron.queue_size 1 \
-    --neuron.sample_size 1 \
-    --neuron.update_interval 60 \
-    --neuron.epoch_length 100 \
-    --protein.pdb_id 1ubq \
-    --wandb.off true \
-    --axon.port 8030 \
-    --neuron.gjp_address 167.99.209.27:8030
+python3 ./neurons/validator.py \
+    --netuid 25 \
+    --subtensor.network finney \
+    --wallet.name <test_coldkey> \
+    --wallet.hotkey <test_hotkey> \
+    --axon.port <your_port> \
