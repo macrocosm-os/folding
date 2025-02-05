49515eabf457c56e868edf4382a64bd818f32ccc
sarkosos <szymonfonau@live.nl>
2025-02-05 12:08:18 +0000

    update update_interval to a random value between 30 minutes and 2 hours

--- folding/store.py
+++ folding/store.py
@@ -280,7 +280,9 @@ class SQLiteJobStore:
             epsilon=epsilon,
             s3_links=s3_links,
             priority=1,
-            update_interval=300,
+            update_interval=random.randint(
+                1800, 7200
+            ),  # between 30 minutes and 2 hours in seconds
             max_time_no_improvement=1,
             **kwargs,
         )
