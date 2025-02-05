8414939210600a55293ac3d30fd406900ecf8ed1
sarkosos <szymonfonau@live.nl>
2025-02-05 11:04:02 +0000

    update default S3 bucket name in config

--- folding/utils/config.py
+++ folding/utils/config.py
@@ -254,7 +254,7 @@ def add_args(cls, parser):
         "--s3.bucket_name",
         type=str,
         help="The name of the S3 bucket to log to.",
-        default="sn25-vali-testnet-bucket",
+        default="vali-s3-demo-do",
     )
 
 
